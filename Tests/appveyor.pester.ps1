5783f8e52f26fe1aa01250bd573ed98022184b92
Andrew Brown <Andrew Brown>
2016-12-09 19:10:44 -0600

    adding specific pester import

--- Tests/appveyor.pester.ps1
+++ Tests/appveyor.pester.ps1
@@ -29,7 +29,8 @@ param(
         "`n`tSTATUS: Testing with PowerShell $PSVersion`n"
         refreshenv
         Get-Module -all
-        Import-Module -Name Pester -Verbose
+        import-Module C:\Program Files\WindowsPowerShell\Modules\Pester\3.4.3\Pester.psm1 -Verbose
+        #Import-Module -Name Pester -Verbose
 
         Invoke-Pester @Verbose -Path "$ProjectRoot\Tests" -OutputFormat NUnitXml -OutputFile "$ProjectRoot\$TestFile" -PassThru |
             Export-Clixml -Path "$ProjectRoot\PesterResults_PS$PSVersion`_$Timestamp.xml"
