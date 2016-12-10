1befc516749b23df772b5aa7f01543579452c66b
Andrew Brown <Andrew Brown>
2016-12-09 19:26:03 -0600

    updated import module path

--- Tests/appveyor.pester.ps1
+++ Tests/appveyor.pester.ps1
@@ -29,8 +29,8 @@ param(
         "`n`tSTATUS: Testing with PowerShell $PSVersion`n"
         #refreshenv
         #Get-Module -all
-        import-Module 'C:\Program Files\WindowsPowerShell\Modules\Pester\3.4.3' -Verbose
-        #Import-Module -Name Pester -Verbose
+        Import-Module "C:\Program Files\WindowsPowerShell\Modules\Pester\3.4.3\bin" -Verbose
+        Import-Module -Name Pester -Verbose
 
         Invoke-Pester @Verbose -Path "$ProjectRoot\Tests" -OutputFormat NUnitXml -OutputFile "$ProjectRoot\$TestFile" -PassThru |
             Export-Clixml -Path "$ProjectRoot\PesterResults_PS$PSVersion`_$Timestamp.xml"
